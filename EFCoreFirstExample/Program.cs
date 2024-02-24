c94f3e4a04aa7c9a37e57914da6da55e19a6a183
vosonha89 <vosonha89@gmail.com>
2024-02-24 20:30:09 +0700

    modify

--- EFCoreFirstExample/Program.cs
+++ EFCoreFirstExample/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 // Tạo thêm UserOrders => 1-n: 1 user có nhiều đơn hàng
 // Tạo thêm Product => 1 table
-// Tạo quan hệ n-n từ UserOrder với Product 
+// Tạo bảng UserOrderProduct (giống bảng product: quantity, note, discount) quan hệ 1-n với UserOrder 
+
 
 using EFCoreFirstExample;
 using EFCoreFirstExample.Entity;
